aa247bbe95149462b0c654c48d5e0ed9b32d4002
RachellMonge <rachellmonge33@gmail.com>
2024-02-02 17:19:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Practica2
+ Rachel_MongeGarita
---
+++ src/test/java/Practica2/PracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
